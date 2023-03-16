a5e69e9c669b5c8a6ccef3aaa837c3d1ab28ff52
AlexSm <alexsm0891@gmail.com>
2023-03-16 16:57:34 +0100

    hw 15.03.2023

--- src/main/java/HW_09_03_2023/TestMyIterator.java
+++ src/main/java/HW_09_03_2023/TestMyIterator.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class TestMyIterator {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        List<Integer> list = new ArrayList<>(List.of(10, 2, 3, 4, 5, 6, 7, 8, 9));
         MyIterator<Integer> myIt = new MyIterator<>(list);
 
         while (myIt.hasNext()) {
@@ -19,6 +19,5 @@ public class TestMyIterator {
             System.out.println(myIt.previous());
         }
         System.out.println(list);
-
     }
 }
